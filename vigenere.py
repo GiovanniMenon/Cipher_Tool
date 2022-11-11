b45e8aaa8bd7d5b9b9c6ba73c4094620e18625ef
GianlucaBresolin <117901582+GianlucaBresolin@users.noreply.github.com>
2022-11-11 17:18:05 +0100

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -4,9 +4,20 @@ def dec_vigenere(text, key):
     flag=''
     for i in range(len(text)):
         if text[i] in string.ascii_lowercase:
-            flag+=''.join(chr((ord(text[i])-ord(key[i%len(key)]))%26+97))
+            flag+=''.join(chr(((ord(text[i])-97)-(ord(key[i%len(key)])-97))%26+97))
         elif text[i] in string.ascii_uppercase:
-            flag+=''.join(chr((ord(text[i])-ord(key[i%len(key)]))%26+65))
+            flag+=''.join(chr(((ord(text[i])-65)-(ord(key[i%len(key)])-65))%26+65))
+        else:
+            flag+=''.join(text[i])
+    return flag
+
+def enc_vigenere(text, key):
+    flag=''
+    for i in range(len(text)):
+        if text[i] in string.ascii_lowercase:
+            flag+=''.join(chr((ord(text[i])-97+ord(key[i%len(key)])-97)%26+97))
+        elif text[i] in string.ascii_uppercase:
+            flag+=''.join(chr((ord(text[i])-65+ord(key[i%len(key)])-65)%26+65))
         else:
             flag+=''.join(text[i])
     return flag
@@ -15,3 +26,5 @@ def dec_vigenere(text, key):
 text="vhixoieemksktorywzvhxzijqni"
 key='caesar'
 print(dec_vigenere(text,key))
+print(enc_vigenere(dec_vigenere(text,key),key))
+
